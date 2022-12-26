a0b3986bf6434d5aa9fb0605aabbcc08bd5a0cda
IrinchinovZh <exp.bur@gmail.com>
2022-12-26 14:40:41 +0800

    create massiv

--- ItogRabota/Program.cs
+++ ItogRabota/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToString(Console.ReadLine()); 
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} \t");
+}
+
+Console.Clear();
+Console.Write("Vvedite kol-vo elem-ov v massive: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array;
+array = new string[n];
+Console.WriteLine("Vvedite elem-ti massiva: ");
+InputArray(array);
+Console.WriteLine("\nMassiv emeet vid: ");
+PrintArray(array);
